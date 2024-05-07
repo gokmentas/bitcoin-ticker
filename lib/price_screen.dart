eb02a149c78f38110203f4391fbee36c58ce3c78
gokmentas <56593104+gokmentas@users.noreply.github.com>
2024-05-07 13:39:19 +0300

    implented dropdownbutton

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   const PriceScreen({super.key});
@@ -8,6 +9,8 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  late String selectedCurrency = "USD";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +48,28 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: [
+                DropdownMenuItem(
+                  child: Text("USD"),
+                  value: "USD",
+                ),
+                DropdownMenuItem(
+                  child: Text("EUR"),
+                  value: "EUR",
+                ),
+                DropdownMenuItem(
+                  child: Text("GPD"),
+                  value: "GPD",
+                ),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value!;
+                });
+              },
+            ),
           ),
         ],
       ),
